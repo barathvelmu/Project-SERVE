201c1109b5feb81b0177fcb053aa3f68c8a0a905
jpnoro2003 <jpnoro2003@gmail.com>
2024-05-18 14:41:33 -0400

    Deleted Unnecessary files

--- project/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from project import views
-
-def create_app():
-    app = Flask(__name__)
-    # Connects our views to the main instance of "app"
-    app.register_blueprint(views.bp) 
-    return app
