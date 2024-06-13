c53cc8a97481dbcde714c97bc2b65f20fece9737
jpnoro2003 <jpnoro2003@gmail.com>
2024-06-13 10:36:45 -0400

    Created new folder for app using Sample DB with new Schema. Rewrote SQL statements for new schema

---
+++ AppNew/pages/_Login.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import requests
+from Home import set_session_tabs
+
+
+#st.set_page_config(page_title="Login", page_icon="🏐")
+st.sidebar.image("logo.png")
+st.sidebar.markdown("<h1 style='text-align: center;'>UW SERVE</h1>", unsafe_allow_html=True)
+
+if 'Login' not in st.session_state:
+    st.session_state['Login'] = False
+
+if 'Email' not in st.session_state:
+    st.session_state['Email'] = False
+
+st.title("SERVE Member Login")
+
+with st.form("Form", clear_on_submit=False):
+    email = st.text_input("Email")
+    if st.form_submit_button("Send Code"):
+        res = requests.get("http://127.0.0.1:5000/sendcode?email="+email).json()
+        st.write(res["status"])
+
+    password = st.text_input("Code (Sent To Email)")
+    if st.form_submit_button("Login with Code"):
+        res = requests.get("http://127.0.0.1:5000/checkpassword?password="+password).json()
+
+        if res["status"] == 1:
+            st.session_state["Login"] = True
+            st.session_state["Email"] = email
+            st.write("Login Successful!")
+        else:
+            st.write("Error. Please try again.")
+
+st.warning("Account will be logged out on refresh.")
+set_session_tabs()
+
+
+
+# https://myaccount.google.com/u/3/apppasswords
+# CS338DATABASE!
