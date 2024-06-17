8dde6693c84467b9ad9bfb5cdb53c4c81be594ab
Nicholas Lin <nicholaszlin.2002@gmail.com>
2024-06-17 01:04:16 -0400

    test

---
+++ OLD SCHEMA/App/api.py
@@ -0,0 +1,122 @@
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+import sqlite3
+import string
+import random
+import smtplib
+from email.mime.text import MIMEText
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    return """You aren't supposed to be here.
+    
+    
+    Leave."""
+
+@app.route('/StudentBreakdown', methods = ['GET', 'POST'])
+def test():
+    con = sqlite3.connect("SERVE.db")
+
+    # Students by Faculty
+    res = con.execute("SELECT Faculty, count(*) from S24_Members group by Faculty")
+    results_faculty = {}
+    for row in res:
+        results_faculty[row[0]] = [row[1]]
+
+    # Students by Gender
+    res = con.execute("SELECT Gender, count(*) from S24_Members group by Gender")
+    results_gender= {}
+    for row in res:
+        results_gender[row[0]] = [row[1]]
+
+    # Students by Year
+    res = con.execute("SELECT YearOfStudy, count(*) from S24_Members group by YearOfStudy")
+    results_year= {}
+    for row in res:
+        results_year[row[0]] = [row[1]]
+        
+    # All Students
+    con.row_factory = sqlite3.Row
+    res = con.execute("SELECT * FROM S24_Members")
+    rows = res.fetchall() # obtain all of the rows from query
+    results_all = []
+    for row in rows:
+        results_all.append(dict(row))
+        
+    con.close()
+
+    return {
+        'faculty': results_faculty, 
+        'gender': results_gender, 
+        'year': results_year,
+        'students': results_all}
+    
+# @app.route('/form', methods = ['GET', 'POST'])
+def form_submit(email, full_name, firstname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest):
+    con = sqlite3.connect("SERVE.db")
+    try:
+        con.execute("INSERT INTO S24_Members (Email, Name, Firstname, StudentNumber, Gender, YearofStudy, Faculty, LevelofPlay, OttawaVNLTripInterest, Completiontime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, strftime('%m/%d/%Y', 'now'))", 
+            (email, full_name, firstname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest))
+        con.commit()
+        return True
+    except:
+        return False
+
+@app.route('/sendcode', methods = ['GET', 'POST'])
+def send_code():
+    # Get email input
+    email = str(request.args.get('email'))
+
+    # Check if email is in S24_Members (count(Email) >= 1)
+    con = sqlite3.connect("SERVE.db")
+    result = con.execute("SELECT count(Email) FROM S24_Members where Email = '%s'" % (str(email))).fetchall()[0][0]
+    con.close()
+    if result == 0:
+        return {'status': "Please Register or contact Admin - Email not detected in database."}
+
+    # Generate and update password in Password (called code)
+    password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=25))
+    con = sqlite3.connect("SERVE.db")
+    result = con.execute("SELECT count(Email) FROM Password where Email = '%s'" % (str(email))).fetchall()[0][0]
+    if result == 0: # Email never logged in, add new row
+        con.execute("INSERT INTO Password (Email, Code) VALUES ('%s', '%s')" % (email,password))
+        con.commit()
+        con.close()
+    else: # Email in table, update password
+        con.execute("UPDATE Password SET code = '%s' WHERE Email = '%s'"%(password, email))
+        con.commit()
+        con.close()
+
+    # if the email is registered send a custom code
+    email = str(request.args.get('email'))
+    subject = "Email Subject"
+    body = "This is your password: " + password
+    sender = "uwservedb@gmail.com"
+    recipients = [email]
+    password = "yuqjdcbrvdhtkqku"
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server: #_SSL 465
+        try:
+            smtp_server.login(sender, password)
+            smtp_server.sendmail(sender, recipients, msg.as_string())
+            return {'status': "Code Sent to Email! This may take a few minutes! Please use it below:"}
+        except Exception as e:
+            return {'status': "Error: " + str(e)}
+
+@app.route('/checkpassword', methods = ['GET', 'POST'])
+def check_password():
+    password = str(request.args.get('password'))
+    con = sqlite3.connect("SERVE.db")
+    result = con.execute("SELECT count(Email) FROM Password where Code = '%s'" % (str(password))).fetchall()[0][0]
+    con.close()
+    if result == 1: # success
+        return {'status': 1}
+    else: # fail
+        return {'status': 0}
---
+++ OLD SCHEMA/App/pages/_Register.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from api import form_submit
+from email_validator import validate_email
+from Home import set_session_tabs
+
+#st.set_page_config(page_title="Form", page_icon="🏐")
+st.sidebar.image("logo.png")
+st.sidebar.markdown("<h1 style='text-align: center;'>UW SERVE</h1>", unsafe_allow_html=True)    
+st.title("SERVE Member Information Form")
+
+if 'Login' not in st.session_state:
+    st.session_state['Login'] = False
+
+if 'Email' not in st.session_state:
+    st.session_state['Email'] = False
+
+def allFieldsCompleted(email, full_name, firstname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest):
+    if (email and full_name and firstname and lastname 
+              and student_id and gender and year_of_study 
+              and faculty and level_of_play and ottawa_trip_interest): 
+        return True
+    else: 
+        return False
+
+
+# DID NOT HANDLE "EvaledLEVEL", "execinitial1", "execinitial2" yet
+if st.session_state["Login"]:
+    st.write("Edit your user information here!")
+else:
+    st.write("Register as a member here!")
+
+with st.form("Form", clear_on_submit=False):
+    if st.session_state["Login"]:
+        email = st.session_state["Email"]
+        st.code(st.session_state["Email"], language="markdown")
+    else:
+        email = st.text_input("Email")
+    full_name = st.text_input("Full Name")
+    firstname = st.text_input("Firstname")
+    lastname = st.text_input("Lastname")
+    student_id = st.number_input("Student Id", step=1, value = None) # Step = 1 used to make integer input only
+    gender = st.selectbox("What is your gender?", ["Male", "Female", "Other"], index = None)
+    year_of_study = st.selectbox("What is your year of study?", ["1st year", "2nd year", "3rd year", "4th year", "5th year", "Alumni", "Graduate Student"], index = None)
+    faculty = st.selectbox("What faculty are you part of?", ["Engineering", "Science", "Health", "Mathematics", "Environment", "Arts"], index = None)
+    level_of_play = st.selectbox("What is your level of play?", ["Level 1", "Level 2", "Level 3", "Level 4"], index = None)
+    ottawa_trip_interest = st.selectbox("Are you willing to go to Ottawa?", ["Yes", "No"], index = None)
+
+    if st.form_submit_button("Submit"):
+        if allFieldsCompleted(email, full_name, firstname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest):
+            if len(str(int(student_id))) == 8:
+                try: 
+                    validate_email(email)
+                    if form_submit(email, full_name, firstname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest):
+                        st.success("Your student information has been added! Please log in through the Login tab.")
+                    else: 
+                        st.error("Update Failed. Please contact an administrator.")
+                except Exception as e:
+                    st.error(f"Email format is invalid. ERROR: '{e}'.")
+            else: 
+                st.error("Student number is invalid. Please enter the 8-digit number.")
+        else:
+            st.error("Please complete all fields.")
+
+            
+set_session_tabs()
