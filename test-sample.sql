ad2e67a1cf47e356bea7fa00f50ff578215aa91f
colinlao <143037375+colinlao@users.noreply.github.com>
2024-06-21 16:35:25 -0400

    Added api.py queries to test-sample.sql

--- test-sample.sql
+++ test-sample.sql
@@ -16,6 +16,42 @@ SELECT Level, Faculty, count(*) from Member group by Faculty, Level
 -- Evaluator Count
 SELECT EvaluatorEmail, count(*) from Member group by EvaluatorEmail
 
+-- Students by Faculty
+SELECT Faculty, count(*)
+from Member
+inner join Member_Valid_For_Term
+on Member.Email = Member_Valid_For_Term.Email
+where Member_Valid_For_Term.Tcode = "S2024"
+group by Faculty
+
+-- Studends by Gender
+SELECT Gender, count(*)
+from Member
+inner join Member_Valid_For_Term
+on Member.Email = Member_Valid_For_Term.Email
+where Member_Valid_For_Term.Tcode = "S2024"
+group by Gender
+
+-- Students by Level
+SELECT coalesce(Level, "Not Assigned") as Assigned_Level, count(*)
+from Member
+inner join Member_Valid_For_Term
+on Member.Email = Member_Valid_For_Term.Email
+where Member_Valid_For_Term.Tcode = "S2024"
+group by Assigned_Level
+
+-- All Students
+select Fname || " " || Lname as Name, Gender, Faculty, Level
+from Member
+inner join Member_Valid_For_Term
+on Member.Email = Member_Valid_For_Term.Email
+where Member_Valid_For_Term.Tcode = "S2024"
+Order by Name
+
+-- Check if email exists
+SELECT count(Email) FROM Member where Email = '%s'
+
+
 -- con.execute("INSERT INTO S24_Members (Email, Name, Firstname, StudentNumber, Gender, YearofStudy, Faculty, LevelofPlay, OttawaVNLTripInterest, Completiontime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, strftime('%m/%d/%Y', 'now'))", 
 --             (email, full_name, firstname, student_id, gender, faculty, level_of_play))
 -- SELECT count(Email) FROM Password where Email = '%s'
