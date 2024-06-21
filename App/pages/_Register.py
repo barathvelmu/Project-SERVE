766c1e0b21bbe6f6ace76cd9c37f76adf0269abc
barathvelmu <barathvelmu@gmail.com>
2024-06-20 22:23:09 -0400

    Removed form validation on disabled fields yearofstudy and ottawatripinterest

--- App/pages/_Register.py
+++ App/pages/_Register.py
@@ -16,10 +16,10 @@ if 'Login' not in st.session_state:
 if 'Email' not in st.session_state:
     st.session_state['Email'] = False
 
-def allFieldsCompleted(email, firstname, lastname, student_id, gender, year_of_study, faculty, level_of_play, ottawa_trip_interest):
+def allFieldsCompleted(email, firstname, lastname, student_id, gender, faculty, level_of_play):
     if (email and firstname and lastname 
-              and student_id and gender and year_of_study 
-              and faculty and level_of_play and ottawa_trip_interest): 
+              and student_id and gender 
+              and faculty and level_of_play): 
         return True
     else: 
         return False
@@ -44,7 +44,7 @@ with st.form("Form", clear_on_submit=False):
     lastname = st.text_input("Lastname")
     student_id = st.number_input("Student Id", step=1, value = None) # Step = 1 used to make integer input only
     gender = st.selectbox("What is your gender?", ["Male", "Female", "Other"], index = None)
-    year_of_study = st.selectbox("What is your year of study?", ["1st year", "2nd year", "3rd year", "4th year", "5th year", "Alumni", "Graduate Student"], index = None)
+    # year_of_study = st.selectbox("What is your year of study?", ["1st year", "2nd year", "3rd year", "4th year", "5th year", "Alumni", "Graduate Student"], index = None)
     faculty = st.selectbox("What faculty are you part of?", ["Engineering", "Science", "Health", "Mathematics", "Environment", "Arts"], index = None)
     level_of_play = st.selectbox("What is your level of play?", ["Level 1", "Level 2", "Level 3", "Level 4"], index = None)
 
