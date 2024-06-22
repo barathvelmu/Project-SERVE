7f95dfccfd9db8eaa1bf68ea201a0ae1521a6d3b
jpnoro2003 <jpnoro2003@gmail.com>
2024-06-21 20:29:16 -0400

    Added FK on delete/update cascade to db and c2 sql file

--- c2-sql-code.sql
+++ c2-sql-code.sql
@@ -17,7 +17,7 @@ CREATE TABLE "Member" (
 	"Faculty"	TEXT NOT NULL,
 	"Level"	TEXT,
 	"EvaluatorEmail"	TEXT,
-	FOREIGN KEY("EvaluatorEmail") REFERENCES "Executive"("Email"),
+	FOREIGN KEY("EvaluatorEmail") REFERENCES "Executive"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Email")
 );
 
@@ -35,7 +35,7 @@ CREATE TABLE "Session" (
 	"EndTime"	TEXT NOT NULL,
 	"Location"	TEXT NOT NULL,
 	"Tcode"	TEXT NOT NULL,
-	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode"),
+	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("EventId")
 );
 
@@ -56,22 +56,22 @@ CREATE TABLE "Team" (
 	"TeamName"	TEXT NOT NULL,
 	"TeamSize"	INTEGER NOT NULL,
 	"EventId"	TEXT NOT NULL,
-	FOREIGN KEY("EventId") REFERENCES "Tournament"("EventId"),
+	FOREIGN KEY("EventId") REFERENCES "Tournament"("EventId") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("TeamId")
 );
 
 CREATE TABLE "Account" (
 	"Password"	TEXT NOT NULL UNIQUE,
 	"Email"	TEXT NOT NULL UNIQUE,
-	FOREIGN KEY("Email") REFERENCES "Member"("Email"),
+	FOREIGN KEY("Email") REFERENCES "Member"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Password","Email")
 );
 
 CREATE TABLE "Member_Valid_For_Term" (
 	"Tcode"	TEXT NOT NULL,
 	"Email"	TEXT NOT NULL,
-	FOREIGN KEY("Email") REFERENCES "Member"("Email"),
-	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode"),
+	FOREIGN KEY("Email") REFERENCES "Member"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Tcode","Email")
 );
 
@@ -79,31 +79,31 @@ CREATE TABLE "Executive_Works_For_Term" (
 	"Tcode"	TEXT NOT NULL,
 	"Email"	TEXT NOT NULL,
 	"ExecTeam"	TEXT NOT NULL,
-	FOREIGN KEY("Email") REFERENCES "Executive"("Email"),
-	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode"),
+	FOREIGN KEY("Email") REFERENCES "Executive"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Tcode") REFERENCES "Term"("Tcode") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Tcode","Email")
 );
 
 CREATE TABLE "Member_Attend_Session" (
 	"Email"	TEXT NOT NULL,
 	"EventId"	TEXT NOT NULL,
-	FOREIGN KEY("EventId") REFERENCES "Session"("EventId"),
-	FOREIGN KEY("Email") REFERENCES "Member"("Email"),
+	FOREIGN KEY("EventId") REFERENCES "Session"("EventId") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("Email") REFERENCES "Member"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Email","EventId")
 );
 
 CREATE TABLE "Member_Make_Team" (
 	"Email"	TEXT NOT NULL,
 	"TeamId"	TEXT NOT NULL,
-	FOREIGN KEY("Email") REFERENCES "Member"("Email"),
-	FOREIGN KEY("TeamId") REFERENCES "Team"("TeamId"),
+	FOREIGN KEY("Email") REFERENCES "Member"("Email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("TeamId") REFERENCES "Team"("TeamId") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("Email","TeamId")
 );
 
 CREATE TABLE "Session_Levels" (
 	"EventId"	TEXT NOT NULL,
 	"Level"	INTEGER NOT NULL,
-	FOREIGN KEY("EventId") REFERENCES "Session"("EventId"),
+	FOREIGN KEY("EventId") REFERENCES "Session"("EventId") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("EventId","Level")
 );
 
@@ -111,5 +111,5 @@ CREATE TABLE "Tournament_Levels" (
 	"EventId"	TEXT NOT NULL,
 	"Level"	INTEGER NOT NULL,
 	PRIMARY KEY("EventId","Level"),
-	FOREIGN KEY("EventId") REFERENCES "Tournament"("EventId")
+	FOREIGN KEY("EventId") REFERENCES "Tournament"("EventId") ON DELETE CASCADE ON UPDATE CASCADE
 );
