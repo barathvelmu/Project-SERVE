3bce8b7ef5fce2e59747ccaace9f3ced991bccdd
colinlao <143037375+colinlao@users.noreply.github.com>
2024-07-04 11:43:58 -0400

    Update test-sample.py with session and tournament api.py queries

--- test-sample.py
+++ test-sample.py
@@ -50,6 +50,37 @@ cursor.execute("""SELECT Fname || " " || Lname as Name, Gender, Faculty, Level
 cursor.execute("SELECT count(Email) FROM Member where Email = '%s'")
 # Check if password exists
 cursor.execute("SELECT count(Email) FROM Account where Password = '%s'")
+# Tournament information
+cursor.execute("""SELECT distinct Tournament.TorneyName, Tournament.Date, Tournament.StartTime, 
+               Tournament.EndTime, Tournament.Location, Tournament.EventId
+    From Tournament""")
+# Member and team information for a specific tournament
+cursor.execute("""SELECT Member.Fname || " " || Member.Lname as Member_Name, Member.Level, Team.TeamName
+    From Team
+    INNER JOIN Tournament
+    ON Team.EventId = Tournament.EventId
+    Inner Join Member_Make_Team
+    on Member_Make_Team.TeamId = Team.TeamId
+    Inner join Member
+    on Member.Email = Member_Make_Team.Email
+    WHERE Tournament.EventId = '%s'
+    """)
+# List of sessions
+cursor.execute("""select session.EventId, session.Date, Session.StartTime, Session.EndTime, Session.Location, 
+               count(Member_Attend_Session.Email) as participant_count, Session_Levels.Level
+    from session
+    inner join Member_Attend_Session
+    on session.EventId = Member_Attend_Session.EventId
+    inner join Session_Levels
+    on Session_Levels.EventId = Session.EventId
+    group by session.EventId
+    """)
+# Check if student is registered to session
+cursor.execute("""select count(*)
+    from Member_Attend_Session
+    where Member_Attend_Session.Email = '%s' and Member_Attend_Session.EventId = '%s'
+    """)
+
 
 rows = cursor.fetchall()
 
